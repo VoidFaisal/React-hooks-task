18b6ac0ae53bd970f0003977e67046ea1f34f32e
VoidFaisal <faisalmuhammad2001@gmail.com>
2024-07-05 17:18:43 +0500

    just removed server line from vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
     proxy: {
       "/api": {
         target: "https://json-server-vercel-taupe.vercel.app/flowerlist",
@@ -12,5 +11,4 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  },
 });
